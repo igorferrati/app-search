d9db432ea13f17b09be1167b0f87f7352d744aaf
igorferrati <ferrati.igor@gmail.com>
2024-04-11 17:03:28 -0300

    search server

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## search-app
+
+Para busca ips:
+go run .\main.go ip --host amazon.com.br     
+
+Para busca servidores:
+go run .\main.go servidores --host amazon.com.br     
+
--- app/app.go
+++ app/app.go
@@ -14,17 +14,25 @@ func Gerar() *cli.App {
 	app.Name = "App cli"
 	app.Usage = "Busca IPs e nome de servidores na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPS de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPS de endereços na internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca nome de servidores na internet",
+			Flags:  flags,
+			Action: buscarServers,
 		},
 	}
 
@@ -45,4 +53,18 @@ func buscarIps(c *cli.Context) {
 		fmt.Println(ip)
 	}
 
+}
+
+func buscarServers(c *cli.Context) {
+	host := c.String("host") //pega flag passada
+
+	servidores, erro := net.LookupNS(host) //server name
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	// para cada servidor
+	for _, servidores := range servidores {
+		fmt.Println(servidores.Host)
+	}
 }
